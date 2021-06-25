e3a7a6dfc7a5d5b9f7f1b95c44d748ab3412076b
MauriceNino <mauriceprivat98@gmail.com>
2021-06-25 17:06:28 +0200

    add more tests for different HTTP verbs

--- Integrationtest/src/main/java/com/gepardec/tdd/models/User.java
+++ Integrationtest/src/main/java/com/gepardec/tdd/models/User.java
@@ -1,21 +1,30 @@
 package com.gepardec.tdd.models;
 
 public class User {
-    private String id;
+    private Long id;
     private String username;
 
     public User() {
+    }
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public User(Long id, String username) {
+        this.id = id;
+        this.username = username;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public String getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
--- Integrationtest/src/test/java/com/gepardec/tdd/rest/UserEndpointTest.java
+++ Integrationtest/src/test/java/com/gepardec/tdd/rest/UserEndpointTest.java
@@ -6,37 +6,144 @@ import org.glassfish.jersey.test.JerseyTest;
 import org.glassfish.jersey.test.TestProperties;
 import org.junit.Test;
 
+import javax.ws.rs.ProcessingException;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserEndpointTest extends JerseyTest {
     @Override
     protected Application configure() {
         enable(TestProperties.LOG_TRAFFIC);
         enable(TestProperties.DUMP_ENTITY);
-        return new ResourceConfig(UserEndpoint.class, User.class);
+        return new ResourceConfig(UserEndpoint.class);
     }
 
     @Test
-    public void testUsers() {
+    public void getUser_works() {
         final Response response = target("users")
-            .path("123")
+            .path("1")
             .request()
             .get();
     
     
-        assertEquals(response.getStatus(), Status.OK.getStatusCode());
-        assertEquals(response.getHeaderString(HttpHeaders.CONTENT_TYPE), MediaType.APPLICATION_JSON);
+        assertEquals(Status.OK.getStatusCode(), response.getStatus());
+        assertEquals(MediaType.APPLICATION_JSON, response.getHeaderString(HttpHeaders.CONTENT_TYPE));
     
         User content = response.readEntity(User.class);
 
         assertNotNull(content);
-        assertEquals(content.getId(), "123");
+        assertEquals(1L, content.getId());
+    }
+
+    @Test
+    public void getUser_badRequest() {
+        final Response response = target("users")
+            .path("someString")
+            .request()
+            .get();
+    
+    
+        assertEquals(Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+    
+        User content = response.readEntity(User.class);
+
+        assertNull(content);
+    }
+
+    @Test
+    public void getUser_notFound() {
+        final Response response = target("users")
+            .path("13")
+            .request()
+            .get();
+    
+        assertEquals(Status.NOT_FOUND.getStatusCode(), response.getStatus());
+    
+        User content = response.readEntity(User.class);
+
+        assertNull(content);
+    }
+
+    @Test
+    public void putUser() {
+        var usr = new User("Test");
+        var entity = Entity.entity(usr, MediaType.APPLICATION_JSON);
+
+        final Response response = target("users")
+            .request()
+            .put(entity);
+    
+        assertEquals(Status.OK.getStatusCode(), response.getStatus());
+    
+        User content = response.readEntity(User.class);
+
+        assertNotNull(content);
+        assertNotNull(content.getId());
+        assertEquals("Test", content.getUsername());
+    }
+
+    @Test
+    public void putUser_badRequest() {
+        var usr = new User();
+        var entity = Entity.entity(usr, MediaType.APPLICATION_JSON);
+
+        final Response response = target("users")
+            .request()
+            .put(entity);
+    
+        assertEquals(Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+    }
+
+    @Test
+    public void deleteUser() {
+        final Response response = target("users")
+            .path("1")
+            .request()
+            .method("DELETE");
+    
+        assertEquals(Status.OK.getStatusCode(), response.getStatus());
+
+        boolean content = response.readEntity(Boolean.class);
+
+        assertTrue(content);
+    }
+
+    /**
+     * Sadly PATCH doesnt work with Jersey.<br/><br/>
+     * 
+     * To try out that the request actually works, deploy it and fire the following HTTP Request:
+     * 
+     * <pre>
+     * PATCH http://127.0.0.1:8080/it/rest/users/1
+     * Content-Type: application/json
+     *
+     * {
+     *    "username": "Test"
+     * }
+     * </pre>
+     */
+    @Test
+    public void patchUser_fail() {
+        var usr = new User("Test");
+        var entity = Entity.entity(usr, MediaType.APPLICATION_JSON);
+
+        try {
+            target("users")
+                .path("1")
+                .request()
+                .method("PATCH", entity);
+        } catch(ProcessingException e) {
+            assertEquals("java.net.ProtocolException: Invalid HTTP method: PATCH", e.getMessage());
+        }
+
     }
 }
