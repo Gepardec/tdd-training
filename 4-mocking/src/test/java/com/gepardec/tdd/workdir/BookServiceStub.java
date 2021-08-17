0c87bb181105423dc3ddc7d4247f8e5ca9458b0d
Julian <julian.gartner@gepardec.com>
2021-08-17 17:01:22 +0200

    add comment

--- 4-mocking/src/test/java/com/gepardec/tdd/workdir/BookServiceStub.java
+++ 4-mocking/src/test/java/com/gepardec/tdd/workdir/BookServiceStub.java
@@ -2,13 +2,24 @@ package com.gepardec.tdd.workdir;
 
 import com.gepardec.tdd.BookService;
 
+import java.util.Collections;
 import java.util.List;
 
 public class BookServiceStub implements BookService {
+
+    /*
+        Test data, so you don't have to come up with it yourself:
+
+        "Pro ASP.NET Core 3: Develop Cloud-Ready Web Applications Using MVC, Blazor, and Razor Pages",
+        "Think Java: How to think like a computer scientist",
+        "Pro WPF in C# 2008",
+        "Java Performance: The Definitive Guide: Getting the Most Out of Your Code",
+        "Learn Python the Hard Way"
+     */
     @Override
     public List<String> retrieveAllBookTitles(String user) {
         // TODO
-        return null;
+        return Collections.emptyList();
     }
 
     @Override
